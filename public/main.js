c3035c3a08b6628f5854819b1241861f045b5df3
Eliska Ryklova <ryklovae@coventry.ac.uk>
2021-11-14 13:17:10 +0000

    Navigation bar improved

--- public/main.js
+++ public/main.js
@@ -32,22 +32,22 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 /**************** Navigation Bar for Small Screens **********************/
-const menuIcon = document.querySelector('nav li:nth-child(2)')
-const closeIcon = document.querySelector('nav li:nth-child(3)')
+const menuIcon = document.querySelector('nav figure:nth-child(2)')
+const closeIcon = document.querySelector('nav figure:nth-child(3)')
 const navItems = document.querySelector('nav ul')
+
+console.log(navItems)
 
 menuIcon.addEventListener("click", () => {
     menuIcon.style.display = "none"
     closeIcon.style.display = "block"
-    for (let i = 3; i < navItems.children.length; i++){ 
-        navItems.children[i].style.display = "block"
-    }
+    navItems.style.display = "block"
+    
 })
 
 closeIcon.addEventListener("click", () => {
     menuIcon.style.display = "block"
     closeIcon.style.display = "none"
-    for (let i = 3; i < navItems.children.length; i++){
-        navItems.children[i].style.display = "none"
-    }
+    navItems.style.display = "none"
+    
 })
